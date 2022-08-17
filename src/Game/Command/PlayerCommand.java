e5d8e4482e526130363a30553b3e584d2890d1b9
UmutcanElci <umutcanelci@yaani.com>
2022-08-17 19:00:24 +0300

    PlayerCommand

---
+++ src/Game/Command/PlayerCommand.java
@@ -0,0 +1,28 @@
+package Game.Command;
+
+import java.util.Scanner;
+
+public class PlayerCommand {
+    public PlayerCommand(){
+        Scanner input = new Scanner(System.in);
+        int choice = 0;
+        while (true){
+            if(choice == 1){
+                //Movement
+            } else if (choice == 2) {
+                //Pass
+            }
+            else if(choice == 3){
+                //Shoot
+            }
+            else if(choice == 4){
+                //Timeout
+            }
+            else {
+                //Wrong input
+                break;
+            }
+
+        }
+    }
+}
