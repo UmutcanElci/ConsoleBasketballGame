37ab19b5d2d5d7b95ba65334c93144e155031380
Umutcan Elci <umutcanelci@yaani.com>
2022-08-21 14:24:02 +0300

    Field progress

--- src/Game/BasketballField.java
+++ src/Game/BasketballField.java
@@ -1,25 +1,10 @@
 package Game;
+import java.awt.desktop.SystemEventListener;
 import java.lang.String;
 public class BasketballField {
    public String field[][] = new String[10][32];
+
     public void printBasketballField(){
-/*
-       for(int i = 0 ; i<10;i++){
-            for(int j = 0; j<32; j++){
-                if(i == 0 || j == 0 ) {
-                    System.out.print(field[i][j] = "T ");
-
-                } else if (i == 9 ||j == 31) {
-                    System.out.print(field[i][j] = "S ");
-                }
-                else {
-                    System.out.print(field[i][j] = " ");
-                }
-
-            }
-            System.out.println();
-        }
- */
         for(int i = 0; i<10; i++){
             for (int j = 0 ; j<32;j++){
                 if(i == 0 || i == 9 || j == 0 || j == 31){
@@ -60,4 +45,27 @@ public class BasketballField {
         }
         
     }
+    public int movePlayer(int a){
+        int row,col;
+        row = 3;
+        col = 3;
+
+        field[row][col] = "P";
+        while (true){
+            if (a == 1){
+                field[row][col+1] = "P";
+            } else if (a == 2) {
+
+            } else if (a == 3) {
+
+            } else if (a == 4) {
+
+            }
+            else {
+                System.out.println("Wrong Input!");
+            }
+        }
+    }
+
 }
+
