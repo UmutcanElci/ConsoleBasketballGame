8fedb2de18839c727f064b9a3ed86235ad5244e4
Umutcan Elci <umutcanelci@yaani.com>
2022-08-24 21:18:54 +0300

    PointGuard

--- src/Game/Players/Position/PointGuard.java
+++ src/Game/Players/Position/PointGuard.java
@@ -7,6 +7,5 @@ public class PointGuard extends BasketballPlayer {
     public PointGuard(String playerName, String playerSurName, int playerAge, double height, double wingspan, double shooting, double blocking) {
         super(playerName, playerSurName, playerAge, height, wingspan, shooting, blocking);
         System.out.println(" Position : PG");
-        System.out.println("Shooting chance : "+ shootingChance(shooting));
     }
 }
